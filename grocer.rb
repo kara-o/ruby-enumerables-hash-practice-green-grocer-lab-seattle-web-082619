35bd98e3bce86137e1621bfafaef125fe820db1d
Kara Odle <karajo.odle@gmail.com>
2019-08-27 13:36:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,33 @@
+require 'pry'
+
+cart = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
+
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+
+  cart.each do |item|
+    item.each do |key, item_info|
+      if cart_hash.key?(key)
+        cart_hash[key][:count] += 1
+      else
+        cart_hash[key] = {
+          :price => item_info[:price],
+          :clearance => item_info[:clearance],
+          :count => 1
+        }
+      end
+    end
+  end
+  
+  cart_hash
 end
+
+consolidate_cart(cart)
 
 def apply_coupons(cart, coupons)
   # code here
